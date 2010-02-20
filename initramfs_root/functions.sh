098b2511b494c07278b4cab53add540daf79cfbe
Piotr Karbowski <jabberuser@gmail.com>
2010-02-20 13:27:43 +0100

    UPDATE readme, info about license and LICENSE.

--- initramfs_root/functions.sh
+++ initramfs_root/functions.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# This code is under Simplified BSD License, see LICENSE for more info
+# Copyright (c) 2010, Piotr Karbowski
+# All rights reserved.
 
 einfo() { echo -ne "\033[1;30m>\033[0;36m>\033[1;36m> \033[0m${@}\n" ;}
 ewarn() { echo -ne "\033[1;30m>\033[0;33m>\033[1;33m> \033[0m${@}\n" ;}
