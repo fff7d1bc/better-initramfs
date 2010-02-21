8870e9522b1a830d2d662bbf5a186297ae4549a6
Piotr Karbowski <jabberuser@gmail.com>
2010-02-21 14:33:18 +0100

    zieff

--- make_initramfs.sh
+++ make_initramfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd initramfs_root && \
-find . | cpio -H newc -o |  gzip > ../initramfs.cpio.gz
+find . | cpio -H newc -o |  gzip -9 > ../initramfs.cpio.gz
 
 if [[ $? == 0 ]] ; then
     echo "Done, copy initramfs.cpio.gz to /boot"
