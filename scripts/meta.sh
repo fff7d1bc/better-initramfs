e29b9b7b28bf42e9ea451229745b51e86b1dccb4
Yamashita TAKAO <tjy1965@gmail.com>
2010-11-15 16:52:42 +0900

    add: rootdelay numeric check and dodebug

--- scripts/meta.sh
+++ scripts/meta.sh
@@ -9,9 +9,21 @@ ewarn "New better-initramfs is not backward compatible, read ChangeLog file.\n"
 
 bin() {
 	einfo 'Preparing binary files...'
-	$workdir/dobin /bin/busybox && ( cd $initramfs_root/bin && if [ ! -h sh ]; then ln -s busybox sh; fi )
+	$workdir/dobin /bin/busybox && \
+		( cd $initramfs_root/bin && if [ ! -h sh ]; then ln -s busybox sh; fi && if [ ! -h bb ]; then ln -s busybox bb; fi)
 	$workdir/dobin /sbin/cryptsetup
 	$workdir/dobin /sbin/lvm.static lvm
+	# add
+	$workdir/dobin /usr/bin/dbclient
+	$workdir/dobin /usr/bin/dbscp
+	$workdir/dobin /sbin/modprobe
+	$workdir/dobin /sbin/modinfo
+	$workdir/dobin /sbin/update-modules
+	$workdir/dobin /sbin/depmod
+	$workdir/dobin /sbin/rmmod
+	$workdir/dobin /sbin/lsmod
+	$workdir/dobin /sbin/insmod
+	
 }
 
 image() {
