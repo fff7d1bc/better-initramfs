b56d3dac93d4e1647a8082a94a3fe4d20601de83
Piotr Karbowski <piotr.karbowski@gmail.com>
2013-05-30 12:46:27 +0200

    Supply TERM=linux if not set.

--- sourceroot/functions.sh
+++ sourceroot/functions.sh
@@ -399,7 +399,7 @@ boot_newroot() {
 	init="${init:-/sbin/init}"
 	einfo "Switching root to /newroot and executing ${init}."
 	if ! [ -x "/newroot/${init}" ]; then die "There is no executable '/newroot/${init}'."; fi
-	exec env -i switch_root /newroot "${init}"
+	exec env -i TERM="${TERM:-linux}" switch_root /newroot "${init}"
 }
 
 emount() {
