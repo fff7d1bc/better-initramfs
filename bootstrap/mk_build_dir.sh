a04bc73bfeda527c40f37cc2ee90b54aeb5275ba
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2021-08-29 23:00:26 +0200

    mk_build_dir.sh: update for new sysroot.

--- bootstrap/mk_build_dir.sh
+++ bootstrap/mk_build_dir.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# This is really a placeholder than a real script.
-# Create Alpine sysroot.
+set -e
+
+# Create Alpine sysroot and pack it into tarball.
 
 export chroot_dir=`pwd`/build_dir/
 export arch=x86_64
 export mirror=http://dl-cdn.alpinelinux.org/alpine/
-export version='2.10.5-r0'
+export version='2.12.7-r0'
 
 mkdir -p \
     "${chroot_dir}" \
@@ -16,18 +17,22 @@ mkdir -p \
 wget "${mirror}/latest-stable/main/${arch}/apk-tools-static-${version}.apk" -O "${chroot_dir}/tmp/apk/apk-tools-static.apk" 
 tar -xzf "${chroot_dir}/tmp/apk/apk-tools-static.apk" -C "${chroot_dir}/tmp/apk/"
 
-"${chroot_dir}/tmp/apk//sbin/apk.static" -X "${mirror}/latest-stable/main" -U --allow-untrusted --root ${chroot_dir} --initdb add \
+"${chroot_dir}/tmp/apk//sbin/apk.static" -X "${mirror}/latest-stable/main" -U --allow-untrusted --root "${chroot_dir}" --initdb add \
     alpine-base \
     build-base \
     patch \
     automake \
     autoconf \
+    cmake \
+    meson \
     pkgconf \
-    linux-headers
+    linux-headers \
+    bash
 
 rm -rf "${chroot_dir}/tmp/apk/"
 
-mknod -m 666 "${chroot_dir}/"dev/null c 1 3
-mknod -m 666 "${chroot_dir}/"dev/urandom c 1 9
-mknod -m 666 "${chroot_dir}/"dev/random c 1 8
-mknod -m 666 "${chroot_dir}/"dev/zero c 1 5
+alpine_version="$(< ${chroot_dir}/etc/alpine-release)"
+alpine_version="${alpine_version//[^0-9.]/}"
+
+export XZ_OPT="-T $(nproc)"
+tar -cvJf "distfiles/alpine-${alpine_version}-x86_64-sysroot.tar.xz" --numeric-owner -C build_dir/ .
